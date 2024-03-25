bc76ff7aa059a9d20c4efe3deac8c104adbbc8df
isaacgomu <IsaacDalglish03@gmail.com>
2024-03-25 13:27:53 +0000

    tested function scopes

---
+++ app.js
@@ -0,0 +1,16 @@
+//console.log("hello world");
+//const myName = "isaac";
+//function sayHello() {
+//console.log("Hello " + myName + "!");
+//}
+
+//sayHello();
+//console.log(myName);
+
+function sayHello() {
+  const myName = "isaac";
+  console.log("Hello " + myName + "!");
+}
+
+sayHello();
+console.log(myName);
